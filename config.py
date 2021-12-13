55349ba5e8586a57ad5bdf13b49b9bb2c7a79693
Nux-xader <sumgmng@gmail.com>
2021-12-13 16:48:51 +0700

    create sent absent notification

--- config.py
+++ config.py
@@ -1,6 +1,27 @@
 ###############################################
 # format : HH:MM
-ABSENT_START = "07:00"
+ABSENT_START = "16:47"
 LATE_ABSENT = "07:45"
 ABSENT_FINISH = "17:00"
+###############################################
+###############################################
+ABSENT_START_MSG = """
+Hai!
+Udah siap kerja belom:V
+Absen yuk supaya jam kerjamu terhitung
+klik hadir atau alpa
+/hadir    /alpa
+"""
+LATE_ABSENT_MSG = f"""
+Waduh kok bisa telat sih absen nya
+catat ini ya jam mulai absen {ABSENT_START} , dan jam telat absen {LATE_ABSENT}
+Lain kali jangan telat absen lagi ya!
+Semangat kerja nyaaaa
+"""
+ABSENT_FINISH_MSG = f"""
+Haiii!
+Udah jam {ABSENT_FINISH} nih, jam kerja kmu udah habis tau
+klik selesaikerja terus istirahat biar pegel pegel nya ilang
+/selesaikerja
+"""
 ###############################################
--- main.py
+++ main.py
@@ -1,36 +1,57 @@
-import telebot, config, json, sys, os
+import telebot, config, time, json, sys, os
+from threading import Thread
 
 bot = telebot.TeleBot("5083702955:AAF7gxDMwaa-fJjTXcvsu6eBoi8A-ZJczvk")
 db_user = "db/users.json"
 
 
 class DB_RW:
-	def __init__(self, path):
-		self.path = path
+    def __init__(self, path):
+        self.path = path
 
-	def dump(self, data):
-	    json.dump(data, open(self.path, 'w'), ensure_ascii=False, indent=4)
+    def dump(self, data):
+        json.dump(data, open(self.path, 'w'), ensure_ascii=False, indent=4)
 
-	def load(self):
-	    return json.load(open(self.path, 'r'))
+    def load(self):
+        return json.load(open(self.path, 'r'))
 
 
 users_data = DB_RW(db_user)
 
+def sent_absent():
+    start, end = True, True
+    while True:
+        t = time.localtime()
+        t = f"{t.tm_hour}:{t.tm_min}"
+        # sending alert start absent
+        if (t == config.ABSENT_START) and (start):
+            start, end = False, True
+            for i in list(users_data.load().keys()):
+                bot.send_message(i, config.ABSENT_START_MSG)
+
+        # sending alert end absent
+        if (t == config.ABSENT_FINISH) and (end):
+            start, end = True, False
+            for i in list(users_data.load().keys()):
+                bot.send_message(i, config.ABSENT_FINISH_MSG)
+        time.sleep(1)
+
+Thread(target=sent_absent).start()
+
 @bot.message_handler(commands=['start'])
 def askname(message):
-	database = users_data.load()
-	if  str(message.chat.id) not in list(database.keys()):
-		bot.send_message(message.chat.id, "Halo!\nMohon kirimkan nama lengkap anda")
+    database = users_data.load()
+    if  str(message.chat.id) not in list(database.keys()):
+        bot.send_message(message.chat.id, "Halo!\nMohon kirimkan nama lengkap anda")
 
 
 @bot.message_handler(func=lambda m: True)
 def save_name(message):
-	database = users_data.load()
-	if  str(message.chat.id) not in list(database.keys()):
-		database[str(message.chat.id)] = str(message.text)
-		users_data.dump(database)
-		bot.send_message(message.chat.id, "Terimakasih\nNama anda telah di daftarkan")
+    database = users_data.load()
+    if  str(message.chat.id) not in list(database.keys()):
+        database[str(message.chat.id)] = str(message.text)
+        users_data.dump(database)
+        bot.send_message(message.chat.id, "Terimakasih\nNama anda telah di daftarkan")
 
 
 
